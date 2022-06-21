ed8e67dc64e16a3426cfed4c4dde4b9a84f59b70
danny-one <88692116+danny-one@users.noreply.github.com>
2022-06-21 16:54:41 -0400

    Add files via upload

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
 module.exports = {
     presets:[
-        "@babel/preset-env",
-        "@babel/preset-react"
-    ]
+		"@babel/typescript",
+			["@babel/preset-env", { "targets": {
+									"node": true 
+									}
+								}]
+	],
 }
